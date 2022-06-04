30569d925e46296899196b713ab2ade601302ca4
MuhammadHasann <muhammadhas772@gmail.com>
2022-06-04 21:19:14 +0700

    Project praktikum 212 Java 6A (hasil)

---
+++ src/frame/PenggunaCreateForm.java
@@ -0,0 +1,93 @@
+package frame;
+
+import model.Pengguna;
+
+import javax.swing.*;
+
+public class PenggunaCreateForm extends JFrame{
+    private JPasswordField tfPassword;
+    private JTextField tfNamaLengkap;
+    private JTextField tfLevel;
+    private JTextField tfUsername;
+    private JButton simpanButton;
+    private JButton batalButton;
+    public JPanel panelMain;
+
+
+    public PenggunaCreateForm() {
+        simpanButton.addActionListener(e -> {
+            Pengguna p = new Pengguna();
+            p.setUsername(tfUsername.getText());
+            p.setPassword(tfPassword.getPassword().toString());
+            p.setNamaLengkap(tfNamaLengkap.getText());
+            p.setLevel(tfLevel.getText());
+            p.create();
+
+            tfUsername.setText("");
+            tfPassword.setText("");
+            tfNamaLengkap.setText("");
+            tfLevel.setText("");
+
+            JOptionPane.showMessageDialog(null, "Simpan data berhasil");
+            dispose();
+            PenggunaViewForm vf = new PenggunaViewForm();
+            vf.setContentPane(vf.panelMain);
+            vf.pack();
+            vf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            vf.setLocationRelativeTo(null);
+            vf.setVisible(true);
+        });
+        batalButton.addActionListener(e -> {
+            dispose();
+            PenggunaViewForm vf = new PenggunaViewForm();
+            vf.setContentPane(vf.panelMain);
+            vf.pack();
+            vf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            vf.setLocationRelativeTo(null);
+            vf.setVisible(true);
+        });
+    }
+
+    public PenggunaCreateForm(int id) {
+        Pengguna p = new Pengguna();
+        p.setId(id);
+        p.find();
+        tfUsername.setText(p.getUsername());
+        tfNamaLengkap.setText(p.getNamaLengkap());
+        tfLevel.setText(p.getLevel());
+
+        simpanButton.addActionListener(e -> {
+            Pengguna pSimpan = new Pengguna();
+            pSimpan.setId(id);
+            pSimpan.setUsername(tfUsername.getText());
+            pSimpan.setPassword(tfPassword.getPassword().toString());
+            pSimpan.setNamaLengkap(tfNamaLengkap.getText());
+            pSimpan.setLevel(tfLevel.getText());
+            pSimpan.update();
+
+            tfUsername.setText("");
+            tfPassword.setText("");
+            tfNamaLengkap.setText("");
+            tfLevel.setText("");
+
+            JOptionPane.showMessageDialog(null, "Ubah data berhasil");
+            dispose();
+            PenggunaViewForm vf = new PenggunaViewForm();
+            vf.setContentPane(vf.panelMain);
+            vf.pack();
+            vf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            vf.setLocationRelativeTo(null);
+            vf.setVisible(true);
+        });
+        batalButton.addActionListener(e -> {
+            dispose();
+            PenggunaViewForm vf = new PenggunaViewForm();
+            vf.setContentPane(vf.panelMain);
+            vf.pack();
+            vf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            vf.setLocationRelativeTo(null);
+            vf.setVisible(true);
+        });
+    }
+
+}
---
+++ src/sql/Main.java
@@ -0,0 +1,25 @@
+package sql;
+
+import frame.PenggunaCreateForm;
+import frame.PenggunaCreateFrame;
+import frame.PenggunaViewForm;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+//        PenggunaCreateFrame frame = new PenggunaCreateFrame();
+//        frame.meletakkanKomponen();
+//        frame.menambahkanListener();
+//        frame.setVisible(true);
+
+
+
+        PenggunaViewForm vf = new PenggunaViewForm();
+        vf.setContentPane(vf.panelMain);
+        vf.pack();
+        vf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        vf.setLocationRelativeTo(null);
+        vf.setVisible(true);
+    }
+}
